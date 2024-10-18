9abbb2c925d7f592315a2f0d0918caaa95a32272
Lab5 <Lab5@LAB1-PC10>
2024-10-18 20:39:34 -0300

    Subida inicial

---
+++ src/main/java/com/mycompany/pruebaml/PruebaML.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pruebaml;
+
+import java.util.*;
+
+/**
+ *
+ * @author Lab5
+ */
+public class PruebaML {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        ArrayList<Integer> ci=new ArrayList<Integer>();
+        
+        ci.add(4695378);
+        
+        System.out.println("Cedula ingresada: "+ ci.get(0));
+        
+        
+    }
+}
